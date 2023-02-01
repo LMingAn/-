371218178ee3d5afc231d9c381c8840425072f9c
MingAn <113867200+LMingAn@users.noreply.github.com>
2023-02-01 16:10:11 +0800

    Add files via upload

---
+++ 成绩分析.c
@@ -0,0 +1,24 @@
+﻿#include<stdio.h>
+
+int main()
+{
+	//输入成绩
+	int students = 0, grade[10000] = { 0 }, n = 0;
+	scanf_s("%d", &students);
+	for (; n < students; n++)
+		scanf_s("%d", &grade[n]);
+
+	//输出成绩
+	int max = grade[0], min = grade[students - 1];
+	float average = grade[0];
+	for (n = 1; n < students; n++)
+	{
+		average += grade[n];
+		if (grade[n] > max)
+			max = grade[n];
+		if (grade[n] < min)
+			min = grade[n];
+	}
+	printf("%d\n%d\n%.2f", max, min, average /= students);
+	return 0;
+}
---
+++ 空间.c
@@ -0,0 +1,12 @@
+﻿#include<stdio.h>
+int main()
+{
+	//1GB = 1024MB
+	//1MB = 1024KB
+	//1KB = 1024B(byte)
+	//1B = 8b(bit)
+	//32位 = 4B -> 32b -> 32 / 8
+	//64位 = 8B -> 64b -> 64 / 8
+	printf("%lld", (long long)256 * 1024 * 1024 * 8 / 32);//加入一个强制类型转换，存放数据
+	return 0;
+}
